3d1b20813a816d861951958e758ddf61ec7a28c0
virajnikam01 <virajnikam01@gmail.com>
2023-10-13 13:51:07 +0530

    title-role script updated

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var theme = document.querySelector(".nav-btn i ")
 var body = document.querySelector("body")
+var title = document.querySelector('#container-one h3 span')
 
 function backToTop(){
     var btn = document.querySelector("footer")
@@ -15,8 +16,8 @@ btn.addEventListener('click', ()=>{
 })
 }
 backToTop()
-
-var dark = false
+function toogleTheme() {
+    var dark = false
 theme.addEventListener("click", ()=>{
    if (!dark) {
     body.classList.add('dark')
@@ -26,3 +27,40 @@ theme.addEventListener("click", ()=>{
     dark = false;
    }
 })
+}
+toogleTheme()
+function autoType() {
+    const wordList = ['Developer.', 'designer.']
+var wordIndex = 0;
+var characterIndex = 0;
+var reverseType = false
+let skipUpdate = 0;
+setInterval(()=>{
+    if (skipUpdate) {
+        skipUpdate--
+        return
+    }
+    if (!reverseType) {
+        skipUpdate = 2
+        title.innerText = title.innerText + wordList[wordIndex][characterIndex]
+    characterIndex++
+
+    }else{
+        title.innerText = title.innerText.slice(0, title.innerText.length-1)
+        characterIndex--
+    }
+    if (characterIndex === wordList[wordIndex].length ) {
+        skipUpdate = 8
+        reverseType = true
+    }
+    if (title.innerText.length === 0 && reverseType) {
+        reverseType = false
+        wordIndex++
+    }
+    if (wordIndex === wordList.length ) {
+        wordIndex = 0
+    }
+    
+}, 60)
+}
+autoType()
